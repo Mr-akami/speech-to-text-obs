29b8f50a578f2e376fae9b307baf1130870c95a1
shun murakami <shun_murakami@earthbrain.com>
2024-02-25 23:02:19 +0900

    chore: add nix flake

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Nix for python";
+
+  inputs = {
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        inherit (nixpkgs.lib) optional;
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          venvDir = "./.venv";
+          buildInputs = [
+            pkgs.python310Packages.python
+            pkgs.python310Packages.venvShellHook
+            pkgs.ffmpeg
+            # this is how we add native dependencies to the shell
+            # e.g. grpc libstdc++.so.6
+            pkgs.stdenv.cc.cc.lib
+          ];
+          
+          
+          #shellHook = ''
+          #'';
+          
+          postVenvCreation = ''
+             unset SOURCE_DATE_EPOCH
+             # pip install -r requirements.txt --editable .
+          '';
+          
+          # Now we can execute any commands within the virtual environment.
+          # This is optional and can be left out to run pip manually.
+          postShellHook = ''
+            if [ -z "$IN_NIX_SHELL_ZSH" ]; then
+              export IN_NIX_SHELL_ZSH=1
+              echo "Starting Zsh..."
+              which zsh
+              ls -l $(which zsh)
+              exec ${pkgs.zsh}/bin/zsh --login || echo "Failed to start Zsh"
+            fi
+            # allow pip to install wheels
+            echo "LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib" >> .venv/bin/activate
+            export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib 
+            unset SOURCE_DATE_EPOCH
+          '';
+        };
+      });
+}
