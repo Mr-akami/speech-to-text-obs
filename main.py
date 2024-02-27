80032eaccd3e499f57e1410a4d7d9900e1ba630e
shun murakami <shun_murakami@earthbrain.com>
2024-02-28 00:23:59 +0900

    test for google recgnition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from textoutput import obsclient
 from speechtotext import speech_recognizer
+import threading
 
 if __name__ == "__main__":
     print("Hello, World!!!!")
@@ -7,7 +8,9 @@ if __name__ == "__main__":
     recognizer = speech_recognizer.SpeechRecognizer()
     # listen_thread = recognizer.make_listen_thread()
     # listen_thread.start()
-    # while True:
-    #     audio = recognizer.pop_front_audio()
-    #     print(audio)
-    #     obs.send_message("Hello World!")
+    threading.Thread(target=recognizer.listen).start()
+    while True:
+        audio = recognizer.pop_front_audio()
+        if audio is not None:
+            print(audio)
+        # obs.send_message("Hello World!")
--- speechtotext/speech_recognizer.py
+++ speechtotext/speech_recognizer.py
@@ -1,5 +1,10 @@
+'''
+change mic priority
+ref: https://qiita.com/chiapis2/items/347a9b422706c2d8ebe2
+'''
 import speech_recognition
 import threading
+import queue
 # import sounddevice
 
 
@@ -12,18 +17,29 @@ class SpeechRecognizer:
         self.recognizer.dynamic_energy_threshold = dynamic_energy_threshold
         self.mic = speech_recognition.Microphone(sample_rate=16000)
         # self.listen_pool = threading.Thread(target=self.listen)
-        self.record_audio_queue = []
+        self.record_audio_queue = queue.Queue()
 
     def listen(self):
+        print("Listening...")
+        print(self.mic)
         with self.mic as source:
+            print(source)
             while True:
                 audio = self.recognizer.listen(source)
-                # print(audio)
+                print(audio)
                 self.record_audio_queue.put_nowait(audio)
                 
     def make_listen_thread(self):
-        return threading.Thread(target=self.listen)
+        return threading.Thread(target=self.listen).start()
 
     def pop_front_audio(self):
-        return self.record_audio_queue.get_nowait()
+        if self.record_audio_queue.qsize() != 0:
+            print("pop_front_audio")
+            try:
+                a = self.recognizer.recognize_google(self.record_audio_queue.get_nowait(), language='ja-JP')
+                print(a)
+            except speech_recognition.UnknownValueError:
+                print("Could not understand audio")
+            # return self.record_audio_queue.get_nowait()
+            
     
